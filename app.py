5f1205246c00f8c928bd35d21e0d44798509969e
sowmiya02102004-droid <sowmiya02102004@gmail.com>
2026-03-16 02:35:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ CORS(app, origins=["https://silly-frangipane-29add5.netlify.app"])
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-pp.config['MAIL_USERNAME'] = "sowmiya02102004@gmail.com"
+app.config['MAIL_USERNAME'] = "sowmiya02102004@gmail.com"
 app.config['MAIL_PASSWORD'] = "itmo qurj yfrf yauw"
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
